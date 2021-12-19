d361ae246f7e15d77fd33a52547ab4f6fb72ce05
Peter Krauß <peter.krauss@kit.edu>
2021-12-19 22:09:17 +0100

    initial commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,14 @@
+<div on:click={console.log}>
+    <slot />
+</div>
+
+<style>
+    div {
+        background-color: red;
+        user-select: none;
+        padding: 10px;
+        display: inline-block;
+        cursor: pointer;
+        border-radius: 10px;
+    }
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Button from './Button.svelte'
+    // const Button = document.require('./Button.svelte')
+
+    let count = 0
+
+    function add() {
+        count += 1
+    }
+
+    function subtract() {
+        count -= 1
+    }
+</script>
+
+<div class="counter">
+    <button on:click={subtract}>-</button>
+    <pre>{count}</pre>
+    <button on:click={add}>+</button>
+</div>
+<div class="message">
+    <slot name="msg" />
+</div>
+<Button>Something</Button>
+
+<style>
+    .counter {
+        display: grid;
+        font-size: 2em;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        margin-top: 2em;
+        place-items: center;
+    }
+    .message {
+        text-align: center;
+    }
+</style>
